54a646b38fab81f14181b7583b6e158474be976c
Samsegun <oyebadesamsegun@gmail.com>
2024-02-16 22:36:23 +0100

    database and prisma crud ops

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,38 +1,74 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
+import prisma from "@/prisma/prisma";
 
-export function GET(
+export async function GET(
     request: NextRequest,
-    { params }: { params: { id: number } }
+    { params }: { params: { id: string } }
 ) {
-    if (params.id > 10)
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(params.id) },
+    });
+
+    if (!user)
         return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-    return NextResponse.json({ id: 1, name: "sam" });
+    return NextResponse.json(user);
 }
 
 export async function PUT(
     request: NextRequest,
-    { params }: { params: { id: number } }
+    { params }: { params: { id: string } }
 ) {
     const body = await request.json();
-    if (!body.name)
+    const validation = schema.safeParse(body);
+
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400 });
+
+    // check if user exists, update and return updated user
+    const user = await prisma.user.findUnique({
+        where: {
+            id: parseInt(params.id),
+        },
+    });
+
+    if (!user)
+        return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+    const updatedUser = await prisma.user.update({
+        where: { id: user.id },
+        data: {
+            name: body.name,
+            email: body.email,
+        },
+    });
+
+    if (!updatedUser)
         return NextResponse.json(
-            { error: "Name is required" },
+            { error: "Failed to update user" },
             { status: 400 }
         );
 
-    if (params.id > 10)
-        return NextResponse.json({ error: "User not found" }, { status: 404 });
-
-    return NextResponse.json({ id: 1, name: body.name });
+    return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(
     request: NextRequest,
-    { params }: { params: { id: number } }
+    { params }: { params: { id: string } }
 ) {
-    if (params.id > 10)
+    const user = await prisma.user.findUnique({
+        where: {
+            id: parseInt(params.id),
+        },
+    });
+
+    if (!user)
         return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-    return NextResponse.json({ id: params.id });
+    const deletedUser = await prisma.user.delete({
+        where: { id: user.id },
+    });
+
+    return NextResponse.json(deletedUser);
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,20 +1,45 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
+import prisma from "@/prisma/prisma";
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: "sam" },
-        { id: 2, name: "tommy" },
-    ]);
+export async function GET(request: NextRequest) {
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
+    const validation = schema.safeParse(body);
 
-    if (!body.name)
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400 });
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: body.email,
+        },
+    });
+
+    if (user) {
         return NextResponse.json(
-            { error: "Name is required" },
+            { error: "User already exists" },
+            { status: 400 }
+        );
+    }
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email,
+        },
+    });
+
+    if (!newUser)
+        return NextResponse.json(
+            { error: "Failed to create user" },
             { status: 400 }
         );
 
-    return NextResponse.json(body, { status: 201 });
+    return NextResponse.json(newUser, { status: 201 });
 }
