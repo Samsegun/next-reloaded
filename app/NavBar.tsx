41734c3ccb167723f7a7d43dac18ae05c4d791b9
Samsegun <oyebadesamsegun@gmail.com>
2024-02-18 21:26:02 +0100

    client auth with next auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { useSession } from "next-auth/react";
 
 const NavBar = () => {
+    const { status, data: session } = useSession();
+
     return (
         <nav className='bg-blue-500 text-white'>
             <ul className='flex flex-1 p-4 justify-between'>
@@ -17,6 +21,16 @@ const NavBar = () => {
                 <li>
                     <Link href={"/products"}>Products</Link>
                 </li>
+
+                {status === "loading" && (
+                    <li className='loading loading-ring'></li>
+                )}
+                {status === "authenticated" && <li>{session.user?.name}</li>}
+                {status === "unauthenticated" && (
+                    <li>
+                        <Link href={"/api/auth/signin"}>Login</Link>
+                    </li>
+                )}
             </ul>
         </nav>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import Loading from "./loading";
+import AuthProvider from "./auth/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +19,11 @@ export default function RootLayout({
     return (
         <html lang='en' data-theme='winter'>
             <body className={inter.className}>
-                <NavBar />
+                <AuthProvider>
+                    <NavBar />
 
-                <main className='p-5'>{children}</main>
+                    <main className='p-5'>{children}</main>
+                </AuthProvider>
             </body>
         </html>
     );
